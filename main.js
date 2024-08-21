d4d9b7dad11d91fb0eec89ed73e9609a68d470fc
steffen.cole.blake@gmail.com <steffen.cole.blake@gmail.com>
2024-08-21 17:53:03 -0600

    3.25.0

--- main.js
+++ main.js
@@ -3,7 +3,20 @@
 const path = require("path");
 const spawnSync = require('child_process').spawnSync;
 
-const pathToMyExe = path.join(__dirname, 'bin', 'PoeFilterX-Standalone-win-x64.exe');
+const platform = process.platform.toLowerCase();
+
+if (platform === 'darwin')
+{
+    throw new Error("No compatible release for Macintosh");
+}
+
+let exeName = 'PoeFilterX-Standalone-linux-x64';
+if (platform === 'win32')
+{
+    exeName = 'PoeFilterX-Standalone-win-x64.exe';
+}
+
+const pathToMyExe = path.join(__dirname, 'bin', exeName);
 const input = process.argv.slice(2);
 
 spawnSync(pathToMyExe, input, { stdio: 'inherit' });
